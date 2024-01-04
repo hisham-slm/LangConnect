dd349d1edb814bdfc31932b8d0ad4c354bd5227a
hisham-slm <hishamsalam350@gmail.com>
2024-01-05 00:49:49 +0530

    add all languages and started speak feature

--- lang_connect/home/static/script.js
+++ lang_connect/home/static/script.js
@@ -1,9 +1,60 @@
-// var translateTo = document.getElementById('translateTo')
-// var language2 = document.getElementById('language2')
-// var selectedLanguage = ''
+$(document).ready(function () {
+    $('#translationForm').submit(function (event) {
+        event.preventDefault()
 
-// language2.addEventListener('change' , function(){
-//     selectedLanguage = this.value
-// })
+        var formData = $(this).serialize()
+        var translatingTo = $('#translatingTo').val()
+        var translatingFrom = $('#translatingFrom').val()
 
-// console.log(selectedLanguage)
+        formData += '&translatingTo=' + translatingTo;
+        formData += '&translatingFrom=' + translatingFrom
+
+        $.ajax({
+            type: 'POST',
+            url: "{% url 'home:translate' %}",
+            data: formData,
+            beforeSend: function () {
+                $('#loader').css('display', 'flex')
+            }
+            ,
+            success: function (response) {
+                $('#loader').css('display', 'none')
+
+                $('#translationResult').html(response.translation);
+                console.log(formData)
+            },
+            error: function (xhr, errmsg, err) {
+                console.log(errmsg);
+            }
+        });
+    })
+
+    $('#pronounciation').submit(function (event) {
+        event.preventDefault()
+
+
+        var formData = $(this).serialize()
+        var word = $('#translationResult').val()
+        var language = $('#translatingTo').val()
+
+
+        formData += '&language=' + language
+        formData += '&word=' + word;
+
+        console.log(formData)
+
+        $.ajax({
+            type: 'POST',
+            url: "{% url 'home:read_out' %}",
+            data: formData,
+            success: function (response) {
+                console.log(formData)
+
+                console.log('working')
+            },
+            error: function (xhr, errmsg, err) {
+                console.log(errmsg);
+            }
+        });
+    })
+})
--- lang_connect/home/views.py
+++ lang_connect/home/views.py
@@ -1,13 +1,25 @@
+import os
 from django.shortcuts import render,redirect
 from django.urls import reverse
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse, FileResponse
 import pyttsx3
 from googletrans import Translator , LANGUAGES
+from gtts import gTTS
 
 
 def home(request):
-    translation = request.GET.get('translation', '')
-    return render(request, 'index.html' , {'translation': translation})
+    
+    language_options = LANGUAGES.items()
+    language_codes = []
+    language_names = []
+    
+    for options in language_options:
+        language_codes.append(options[0])
+        language_names.append(options[1].capitalize())
+    
+    zipped_language = zip(language_codes, language_names)
+    zipped_language2 = zip(language_codes, language_names)
+    return render(request, 'index.html' , {'zipped_language':zipped_language , 'zipped_language2':zipped_language2})
 
 def play(request):
     engine = pyttsx3.init()
@@ -27,6 +39,34 @@ def translate(request):
         translating_from = request.POST['translatingFrom']
         word = request.POST['word']
         translator = Translator()
+        print(word)
         translation = translator.translate(word,dest=language,src=translating_from).text
         
         return JsonResponse({'translation' : translation})
+
+def read_out(request):
+    if request.method == 'POST':
+        # word = request.POST['word']
+        # print(word)
+        
+        # engine = pyttsx3.init()
+        
+        # engine.say(word)
+        # engine.runAndWait()
+        
+        # return {'working'}
+        
+        language = request.POST['language']
+        
+        word = request.POST['word']
+        
+        speech = gTTS(lang = language, text= word) 
+        audio_file = 'test.mp3'
+        speech.save(audio_file)
+        
+        
+        try:
+            return FileResponse(open(audio_file, 'rb'), content_type='audio/mpeg')
+        except FileNotFoundError:
+            return HttpResponse("Audio file not found", status=404)
+    
